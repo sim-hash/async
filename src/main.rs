645f1ec1663fc6c994e332ee863763a8a085f2a6
sim-hash <mariussiminic@protonmail.com>
2025-01-28 23:59:36 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,91 @@ mod ffi;
 mod poll;
 mod bitmask;
 
-use std::{io::{Write, Result, Read, ErrorKind}, net::TcpStream, env};
 
 use ffi::Event;
+use mio::{Token, Interest};
 use poll::Poll;
+use std::{io::{Write, Result, Read, ErrorKind, self}, net::TcpStream, env, collections::HashSet, usize};
 
 fn main() -> Result<()> {
-    let mut poll = Poll::new()?;
+    poll_events_mio_impl();
+    Ok(())
+}
+
+fn poll_events_mio_impl() ->Result<()> {
+    let mut poll = mio::Poll::new()?;
     let n_events = 5;
  
-    let mut stream = vec![];
+    let mut streams = vec![];
     let base_url = env::args().nth(1).unwrap_or_else(|| String::from("localhost"));
 
     let addr = format!("{}:8080", &base_url);
     for i in 0..n_events {
         let delay = (n_events - 1) * 1000;
-        let url_path = format!("/{delay}request-{1}");
+        let url_path = format!("/{delay}/request-{i}");
         let request = get_req(&url_path);
         let std_stream = std::net::TcpStream::connect(&addr)?;
-        std_stream.set_nonblocking(true)?
+        std_stream.set_nonblocking(true)?;
+         
+        let mut stream = mio::net::TcpStream::from_std(std_stream);
+        stream.write_all(request.as_bytes())?;
 
-
+        poll.registry().register(&mut stream, Token(i), Interest::READABLE)?;
+        streams.push(stream);
     }
 
+    let mut handled_ids = HashSet::new();
+    let mut handled_events = 0;
+
+    while handled_events < n_events {
+        let mut events = mio::Events::with_capacity(10);
+        poll.poll(&mut events, None);
+        
+        if events.is_empty() {
+            println!("TIMEOUT (OR SPURIOUS EVENT NOTIFICATION)");
+            continue;
+        }
+
+        let events: Vec<mio::event::Event> = events.into_iter().map(|e| e.clone()).collect();
+        handled_events += handle_events_mio_impl(&events, &mut streams, &mut handled_ids)?;
+    }
+
+    println!("Finished");
     Ok(())
 }
 
-fn poll_events() -> Result<()> {
+fn handle_events_mio_impl(events: &[mio::event::Event], streams: &mut [mio::net::TcpStream], handled: &mut HashSet<usize>) -> Result<usize> {
+    let mut handled_events = 0;
+    for event in events {
+        // extract value wrapped in token
+        let index: usize = event.token().into();
+        let mut data = vec![0u8; 4096];
+        loop {
+            match streams[index].read(&mut data) {
+                Ok(n) if n == 0 => {
+                    if !handled.insert(index) {
+                        break;
+                    }
+
+                    handled_events += 1;
+                    break
+                }
+                Ok(n) => {
+                    let txt = String::from_utf8_lossy(&data[..n]);
+
+                    println!("RECEIVED: {:?}", event);
+                    println!("{txt}\n--------\n");
+                }
+                Err(e) if matches!(e.kind(), io::ErrorKind::WouldBlock | io::ErrorKind::Interrupted) => break,
+                Err(e) => return Err(e),
+            }
+        }
+    }
+
+    Ok(handled_events)
+}
+
+fn poll_events_standard_implementation() -> Result<()> {
     let mut poll = Poll::new()?;
     let n_events = 5;
 
@@ -58,13 +116,13 @@ fn poll_events() -> Result<()> {
             continue;
         }
 
-        handled_events += handle_events(&events, &mut streams)?;
+        handled_events += handle_events_std_impl(&events, &mut streams)?;
     }
 
     Ok(())
 }
 
-fn handle_events(events: &[Event], streams: &mut [TcpStream]) -> Result<usize> {
+fn handle_events_std_impl(events: &[Event], streams: &mut [TcpStream]) -> Result<usize> {
     let mut handled_events = 0;
 
     for event in events {
